e07c9bce88feeeae6b8256b2d67717317efa305f
Brandioo <66759355+Brandioo@users.noreply.github.com>
2022-06-19 04:33:13 +0200

    Update Run.php

--- Run.php
+++ Run.php
@@ -4,6 +4,6 @@ $text = ["Card Identity", "Other Document"];
 foreach ($text as $value) {
     $replaceWord = lcfirst(str_replace(" ", "", ucwords(strtolower($value), " ")));
     $replaceWord = preg_replace("/[^A-Za-z0-9 ]/", '', $replaceWord);
-    echo "UPDATE `iDocIP`.`DocumentList` SET `iDocIP`.`DocumentList`.`document_identifier` = \"$replaceWord\" WHERE TRIM(`iDocIP`.`DocumentList`.`documentName`) = \"$value\"\n";
+    echo "UPDATE ``.`` SET ``.``.`_` = \"$replaceWord\" WHERE TRIM(``.``.``) = \"$value\"\n";
 }
 ?>
